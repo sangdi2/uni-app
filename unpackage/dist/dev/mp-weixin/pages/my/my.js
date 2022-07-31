c231af7912e99134134f337d1deb3b0c8b573fd2
sangdi <1592807515@qq.com>
2022-07-31 11:19:29 +0800

    grd

---
+++ unpackage/dist/dev/mp-weixin/pages/my/my.js
@@ -0,0 +1,12 @@
+"use strict";
+var common_vendor = require("../../common/vendor.js");
+const _sfc_main = {
+  data() {
+    return {};
+  }
+};
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return {};
+}
+var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "C:/Users/sangdi/Desktop/vs\u7EC3\u4E60/uni-app\u5FAE\u4FE1\u5C0F\u7A0B\u5E8F/uni_shop/pages/my/my.vue"]]);
+wx.createPage(MiniProgramPage);
