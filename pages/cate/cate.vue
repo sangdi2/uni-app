fc024cfbefb3e32fd326619c97a9d95a9295fe0a
sangdi <1592807515@qq.com>
2022-08-11 09:51:47 +0800

    fdh

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,19 +1,120 @@
 <template>
 	<view>
-		cate---
+		<view class="scroll">
+			<scroll-view class="left-scroll" scroll-y="true" :style="{height:windowHeight+'px'}">
+				<view class="cate1" v-for="(item,i) in cate1List" :key="i" @click="cateChange(i)">
+				<view :class="['left-scroll-item',active==i?'active':'']">{{item.cat_name}}</view>
+				</view>
+			</scroll-view>
+			<scroll-view class="right-scroll" scroll-y="true" :style="{height:windowHeight+'px'}" :scroll-top="scrollTop">
+				<view class="right-scroll-title" v-for="(item1,i1) in cate2List" :key="i2">
+					<view class="right-scroll-title-item">/{{item1.cat_name}}/</view>
+					<view class="right-scroll-content">
+						<view class="right-scroll-content-item" v-for="(item2,i2) in item1.children" :key="i2" @click="gotoGoodsList(item2)">
+							<image :src="item2.cat_icon"></image>
+							<text>{{item2.cat_name}}</text>
+						</view>
+					</view>
+				</view>
+			</scroll-view>
+		</view>
 	</view>
 </template>
 
 <script>
+	import {$http} from '@escook/request-miniprogram'
 	export default {
 		data() {
 			return {
-				
+				windowHeight:0,
+				cate1List:[],
+				active:0,
+				cate2List:[],
+				scrollTop:0
 			};
+		},
+		onLoad() {
+			const wh =uni.getSystemInfoSync()
+			console.log(wh)
+			this.windowHeight=wh.windowHeight
+			this.getcateList()
+		},
+		methods:{
+			async getcateList(){
+				const {data:res} = await $http.get("https://api-ugo-web.itheima.net/api/public/v1/categories")
+				console.log(res)
+				if(res.meta.status!=200){
+					return uni.$showMeg()
+				}
+				this.cate1List=res.message;
+				this.cate2List=res.message[0].children
+			},
+			cateChange(i){
+				this.active=i
+				this.cate2List=this.cate1List[i].children
+				this.scrollTop=this.scrollTop===0?1:0
+			},
+			gotoGoodsList(item2){
+				uni.navigateTo({
+					url:'/subpkg/goods_list/goods_list?cid='+item2.cat_id
+				})
+			}
 		}
+		
 	}
 </script>
 
 <style lang="scss">
-
+.scroll{
+	display: flex;
+	.left-scroll{
+		width: 200rpx;
+		.left-scroll-item{
+			height: 100rpx;
+			background-color: #fff;
+			text-align: center;
+			font-size: 25rpx;
+			line-height: 100rpx;
+		}
+		.active {
+			background-color: #f3f3f3;
+			position: relative;
+		}
+		.active::before{
+			content: '';
+			width: 6rpx;
+			height: 60rpx;
+			background-color: #c00000;
+			position: absolute;
+			top:50%;
+			left: 0;
+			transform: translateY(-50%);
+		}
+	}
+	.right-scroll-title-item{
+		text-align: center;
+		font-size: 25rpx;
+		font-weight: 800;
+		margin: 20px 0;
+	}
+	.right-scroll-content{
+		display: flex;
+		flex-wrap: wrap;
+		.right-scroll-content-item{
+			width: 33.33%;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			image{
+				width: 60px;
+				height: 60px;
+			}
+			text{
+				font-size: 28rpx;
+				
+			}
+		}
+	}
+}
 </style>
--- unpackage/dist/dev/mp-weixin/pages/cate/cate.js
+++ unpackage/dist/dev/mp-weixin/pages/cate/cate.js
@@ -2,11 +2,70 @@
 var common_vendor = require("../../common/vendor.js");
 const _sfc_main = {
   data() {
-    return {};
+    return {
+      windowHeight: 0,
+      cate1List: [],
+      active: 0,
+      cate2List: [],
+      scrollTop: 0
+    };
+  },
+  onLoad() {
+    const wh = common_vendor.index.getSystemInfoSync();
+    console.log(wh);
+    this.windowHeight = wh.windowHeight;
+    this.getcateList();
+  },
+  methods: {
+    async getcateList() {
+      const { data: res } = await common_vendor.$http.get("https://api-ugo-web.itheima.net/api/public/v1/categories");
+      console.log(res);
+      if (res.meta.status != 200) {
+        return common_vendor.index.$showMeg();
+      }
+      this.cate1List = res.message;
+      this.cate2List = res.message[0].children;
+    },
+    cateChange(i) {
+      this.active = i;
+      this.cate2List = this.cate1List[i].children;
+      this.scrollTop = this.scrollTop === 0 ? 1 : 0;
+    },
+    gotoGoodsList(item2) {
+      common_vendor.index.navigateTo({
+        url: "/subpkg/goods_list/goods_list?cid=" + item2.cat_id
+      });
+    }
   }
 };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
-  return {};
+  return {
+    a: common_vendor.f($data.cate1List, (item, i, i0) => {
+      return {
+        a: common_vendor.t(item.cat_name),
+        b: common_vendor.n($data.active == i ? "active" : ""),
+        c: i,
+        d: common_vendor.o(($event) => $options.cateChange(i))
+      };
+    }),
+    b: $data.windowHeight + "px",
+    c: common_vendor.f($data.cate2List, (item1, i1, i0) => {
+      return {
+        a: common_vendor.t(item1.cat_name),
+        b: common_vendor.f(item1.children, (item2, i2, i12) => {
+          return {
+            a: item2.cat_icon,
+            b: common_vendor.t(item2.cat_name),
+            c: i2,
+            d: common_vendor.o(($event) => $options.gotoGoodsList(item2))
+          };
+        })
+      };
+    }),
+    d: _ctx.i2,
+    e: $data.windowHeight + "px",
+    f: $data.scrollTop
+  };
 }
 var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "C:/Users/sangdi/Desktop/vs\u7EC3\u4E60/uni-app\u5FAE\u4FE1\u5C0F\u7A0B\u5E8F/uni_shop/pages/cate/cate.vue"]]);
 wx.createPage(MiniProgramPage);
