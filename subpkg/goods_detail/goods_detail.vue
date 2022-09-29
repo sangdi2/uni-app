edf1b82a335416da3d7cf2e8fdf99327ac7173aa
sangdi <1592807515@qq.com>
2022-09-29 13:04:16 +0800

    h

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,19 +1,146 @@
 <template>
 	<view>
-		
+		<swiper class="swiper" circular :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval"
+						:duration="duration">
+						<swiper-item v-for="(item,i) in detail_info.pics" :key="i">
+							<image :src="item.pics_big" @click="preview(i)"></image>
+						</swiper-item>
+						
+		</swiper>
+		<view class="detail-info">
+			<view class="price">￥{{detail_info.goods_price}}</view>
+			<view class="info-box">
+				<view class="name">{{detail_info.goods_name}}</view>
+				<view class="scbox">
+					<uni-icons type="star" size="15" color="gray"></uni-icons>
+					<view>收藏</view>
+				</view>
+			</view>
+			<view class="yf">快递:免运费</view>
+		</view>
+		<view class="goodsnav">
+			<uni-goods-nav :fill="true"  :options="options" :buttonGroup="buttonGroup"  @click="onClick" @buttonClick="buttonClick" />
+		</view>
 	</view>
 </template>
 
 <script>
+	import {$http} from '@escook/request-miniprogram'
 	export default {
 		data() {
 			return {
-				
+				detail_info:{},
+				indicatorDots: true,
+				autoplay: true,
+				interval: 2000,
+				duration: 500,
+				options: [ {
+				            icon: 'shop',
+				            text: '店铺',
+				           
+				            infoBackgroundColor:'#007aff',
+				            infoColor:"red"
+				        }, {
+				            icon: 'cart',
+				            text: '购物车',
+				            
+				        }],
+				buttonGroup: [{
+				          text: '加入购物车',
+				          backgroundColor: '#ff0000',
+				          color: '#fff'
+				        },
+				        {
+				          text: '立即购买',
+				          backgroundColor: '#ffa200',
+				          color: '#fff'
+				        }
+				        ]
 			};
+		},
+		onLoad(options){
+			const goods_id=options.goods_id
+			this.getgoodsdetails(goods_id)
+		},
+		methods:{
+			async getgoodsdetails(goods_id){
+				const {data:res} =await $http.get('https://api-ugo-web.itheima.net/api/public/v1/goods/detail',{goods_id})
+				console.log(res)
+				if(res.meta.status!==200){
+					return uni.$showMeg()
+				}
+				this.detail_info=res.message
+			},
+			preview(i){
+				uni.previewImage({
+					current:i,
+					urls:this.detail_info.pics.map(x=>x.pics_big)
+				})
+			},
+			onClick(e){
+				console.log(e)
+				if(e.content.text=='购物车'){
+					uni.switchTab({
+						url:'/pages/cart/cart'
+					})
+				}
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
-
+.swiper{
+	height: 750rpx;
+	swiper-item{
+		image{
+			width: 100%;
+			height: 100%
+		}
+	}
+}
+.detail-info{
+	padding: 10px;
+	.price{
+		font-size: 15px;
+		color: red;
+		margin-bottom: 10px;
+	}
+	.info-box{
+		display: flex;
+		justify-content: space-between;
+		.name{
+			font-size: 13px;
+			margin-right: 10px;
+		}
+		.scbox{
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			color: gray;
+			border-left: 1px solid #efefef;
+			padding: 0 5px;
+			view{
+				font-size: 10px;
+				width: 20px;
+			}
+		}
+	}
+	.yf{
+		font-size: 12px;
+		color: gray;
+		margin-top: 10px;
+	}
+}
+.goodsnav{
+	width: 100%;
+	position: fixed;
+	bottom: 0;
+	left: 0;
+	uni-goods-nav{
+		width: 100%;
+		height: 100%;
+	}
+}
 </style>
--- unpackage/dist/dev/mp-weixin/subpkg/goods_detail/goods_detail.js
+++ unpackage/dist/dev/mp-weixin/subpkg/goods_detail/goods_detail.js
@@ -2,11 +2,102 @@
 var common_vendor = require("../../common/vendor.js");
 const _sfc_main = {
   data() {
-    return {};
+    return {
+      detail_info: {},
+      indicatorDots: true,
+      autoplay: true,
+      interval: 2e3,
+      duration: 500,
+      options: [{
+        icon: "shop",
+        text: "\u5E97\u94FA",
+        infoBackgroundColor: "#007aff",
+        infoColor: "red"
+      }, {
+        icon: "cart",
+        text: "\u8D2D\u7269\u8F66"
+      }],
+      buttonGroup: [
+        {
+          text: "\u52A0\u5165\u8D2D\u7269\u8F66",
+          backgroundColor: "#ff0000",
+          color: "#fff"
+        },
+        {
+          text: "\u7ACB\u5373\u8D2D\u4E70",
+          backgroundColor: "#ffa200",
+          color: "#fff"
+        }
+      ]
+    };
+  },
+  onLoad(options) {
+    const goods_id = options.goods_id;
+    this.getgoodsdetails(goods_id);
+  },
+  methods: {
+    async getgoodsdetails(goods_id) {
+      const { data: res } = await common_vendor.$http.get("https://api-ugo-web.itheima.net/api/public/v1/goods/detail", { goods_id });
+      console.log(res);
+      if (res.meta.status !== 200) {
+        return common_vendor.index.$showMeg();
+      }
+      this.detail_info = res.message;
+    },
+    preview(i) {
+      common_vendor.index.previewImage({
+        current: i,
+        urls: this.detail_info.pics.map((x) => x.pics_big)
+      });
+    },
+    onClick(e) {
+      console.log(e);
+      if (e.content.text == "\u8D2D\u7269\u8F66") {
+        common_vendor.index.switchTab({
+          url: "/pages/cart/cart"
+        });
+      }
+    }
   }
 };
+if (!Array) {
+  const _easycom_uni_icons2 = common_vendor.resolveComponent("uni-icons");
+  const _easycom_uni_goods_nav2 = common_vendor.resolveComponent("uni-goods-nav");
+  (_easycom_uni_icons2 + _easycom_uni_goods_nav2)();
+}
+const _easycom_uni_icons = () => "../../uni_modules/uni-icons/components/uni-icons/uni-icons.js";
+const _easycom_uni_goods_nav = () => "../../uni_modules/uni-goods-nav/components/uni-goods-nav/uni-goods-nav.js";
+if (!Math) {
+  (_easycom_uni_icons + _easycom_uni_goods_nav)();
+}
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
-  return {};
+  return {
+    a: common_vendor.f($data.detail_info.pics, (item, i, i0) => {
+      return {
+        a: item.pics_big,
+        b: common_vendor.o(($event) => $options.preview(i)),
+        c: i
+      };
+    }),
+    b: $data.indicatorDots,
+    c: $data.autoplay,
+    d: $data.interval,
+    e: $data.duration,
+    f: common_vendor.t($data.detail_info.goods_price),
+    g: common_vendor.t($data.detail_info.goods_name),
+    h: common_vendor.p({
+      type: "star",
+      size: "15",
+      color: "gray"
+    }),
+    i: common_vendor.o($options.onClick),
+    j: common_vendor.o(_ctx.buttonClick),
+    k: common_vendor.p({
+      fill: true,
+      options: $data.options,
+      buttonGroup: $data.buttonGroup
+    })
+  };
 }
 var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "C:/Users/sangdi/Desktop/vs\u7EC3\u4E60/uni-app\u5FAE\u4FE1\u5C0F\u7A0B\u5E8F/uni_shop/subpkg/goods_detail/goods_detail.vue"]]);
 wx.createPage(MiniProgramPage);
